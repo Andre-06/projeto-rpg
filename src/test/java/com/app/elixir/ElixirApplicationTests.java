1af85796de8d1289b7cb560cdb7422089aae29ad
Laura Daguer <laura.daguerds@gmail.com>
2023-06-14 08:35:11 -0300

    Criação do Projeto

---
+++ src/test/java/com/app/elixir/ElixirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.elixir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElixirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
